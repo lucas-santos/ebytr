0e7109c332f617a2dde3fa72a16dee68291f4f92
Lucas Santos <lucassantos_dev@outlook.com>
2022-05-14 18:33:05 -0300

    estrutura do projeto

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("okay");
+app.get('/', (req, res) => {
+  res.send('okay');
 });
 
 app.listen(3000, () => {
-  console.log("Escutando na porta 3000");
+  console.log('Escutando na porta 3000');
 });
